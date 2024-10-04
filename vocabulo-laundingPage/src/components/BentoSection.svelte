81fa3d09a80f0768704b8dd5a5b88e4cc688538d
unknown <5628@holbertonstudents.com>
2024-10-04 14:28:27 +0200

    add icon and image for bento kid

--- vocabulo-laundingPage/src/components/BentoSection.svelte
+++ vocabulo-laundingPage/src/components/BentoSection.svelte
@@ -27,30 +27,48 @@
     $: bgImage1 = $isQuizMode ? "" : "";
     $: bgImage2 = $isQuizMode
         ? "assets/bento/4Quiz.png"
-        : "assets/bento/4Quiz.png";
+        : "assets/bento/4Kid.png";
     $: bgImage3 = $isQuizMode
         ? "assets/bento/3Quiz.png"
         : "assets/bento/3Quiz.png";
     $: bgImage5 = $isQuizMode
         ? "assets/bento/5Quiz.png"
-        : "assets/bento/5Quiz.png";
+        : "assets/bento/5Kid.png";
 
     // Reactive SVG icons
     $: icon1 = $isQuizMode
-        ? `<svg xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" width="18" height="18" x="0" y="0" viewBox="0 0 96 96" style="enable-background:new 0 0 512 512" xml:space="preserve" class=""><g><path fill="#efcbbd" d="m43.295 62.332-12.566-8.814a6.388 6.388 0 1 0-7.634 10.244s3.601 3.115 5.048 4.91c5.644 7 19.801 23.42 38.218 14.886 6.271-2.906 11.431-6.857 13.739-12.871 2.309-6.014 2.403-12.937-.41-19.246L71.6 33.696l-.454-.999-5.475-12.28a5.32 5.32 0 0 0-9.716 4.332l7.449 17.221-11.513-25.825a5.021 5.021 0 0 0-6.631-2.541l-.544.243a5.021 5.021 0 0 0-2.541 6.631L52.71 44.064 39.576 14.607a5.021 5.021 0 0 0-6.631-2.541l-.544.243a5.021 5.021 0 0 0-2.541 6.631l2.416 5.418 10.293 22.891-9.491-21.289a5.021 5.021 0 0 0-6.631-2.541l-.544.243a5.021 5.021 0 0 0-2.541 6.631l11.737 26.324z" opacity="0" data-original="#efcbbd" class=""></path><path fill="#ffffff" d="M55.944 87.884c-4.834 0-9.67-1.434-14.465-4.301-6.456-3.859-11.315-9.354-14.827-13.71-1.079-1.338-3.755-3.748-4.762-4.622a8.248 8.248 0 0 1-3.194-5.413 8.245 8.245 0 0 1 1.559-6.16c2.428-3.259 6.808-4.224 10.329-2.486l-8.97-20.119a6.945 6.945 0 0 1 3.511-9.16l.545-.243a6.846 6.846 0 0 1 3.045-.602l-.602-1.35a6.945 6.945 0 0 1 3.51-9.16l.545-.243c3.493-1.557 7.602.017 9.159 3.51l.086.193a6.89 6.89 0 0 1 2.525-1.923l.545-.243c3.493-1.557 7.602.017 9.159 3.51l1.323 2.966a7.19 7.19 0 0 1 2.905-2.355c1.765-.787 3.731-.839 5.535-.148s3.231 2.044 4.018 3.809l5.923 13.267 8.088 17.741c2.917 6.542 3.077 13.898.456 20.726-2.231 5.812-7.047 10.366-14.722 13.922-3.728 1.731-7.475 2.594-11.224 2.594zm-29.04-33.709a4.449 4.449 0 0 0-3.579 1.792c-.714.958-1.012 2.136-.839 3.318s.795 2.226 1.753 2.94c.037.027.074.057.109.087.152.132 3.749 3.25 5.286 5.157 4.5 5.581 18.192 22.565 35.922 14.35 6.727-3.117 10.9-6.983 12.757-11.82 2.254-5.872 2.122-12.187-.371-17.779l-8.538-18.731-5.481-12.292c-.37-.83-1.041-1.467-1.89-1.792s-1.774-.3-2.605.07a3.407 3.407 0 0 0-1.722 4.495l7.458 17.24a1.916 1.916 0 0 1-3.506 1.54L50.142 16.925a3.11 3.11 0 0 0-4.102-1.572l-.545.243a3.113 3.113 0 0 0-1.688 3.802l10.651 23.888a1.916 1.916 0 0 1-3.498 1.561l-.006-.013-10.528-23.576a7.309 7.309 0 0 1-.181-.446l-2.419-5.425a3.11 3.11 0 0 0-4.101-1.572l-.545.243a3.11 3.11 0 0 0-1.572 4.101l2.722 6.1c.186.292.352.599.496.921l9.491 21.288a1.915 1.915 0 0 1-3.496 1.565l-9.844-21.892a3.077 3.077 0 0 0-1.375-1.037 3.093 3.093 0 0 0-2.376.064l-.545.243a3.11 3.11 0 0 0-1.572 4.101L36.61 55.305l7.784 5.459a1.915 1.915 0 0 1-2.195 3.139l-8.196-5.715a1.923 1.923 0 0 1-.254-.213l-4.121-2.89a4.544 4.544 0 0 0-2.724-.91z" opacity="1" data-original="#16191e" class=""></path><path fill="#768dff" d="M77.944 10.031a45.419 45.419 0 0 1 12.14 17.178" opacity="1" data-original="#768dff" class=""></path><path fill="#ffffff" d="M90.085 29.125c-.76 0-1.479-.455-1.779-1.204a43.684 43.684 0 0 0-11.629-16.453 1.915 1.915 0 1 1 2.532-2.874 47.538 47.538 0 0 1 12.652 17.903 1.915 1.915 0 0 1-1.776 2.628z" opacity="1" data-original="#16191e" class=""></path><path fill="#768dff" d="M6.601 50.791a45.102 45.102 0 0 1 2.101-23.506" opacity="1" data-original="#768dff" class=""></path><path fill="#ffffff" d="M6.599 52.706a1.916 1.916 0 0 1-1.884-1.584 47.209 47.209 0 0 1 2.19-24.501 1.915 1.915 0 1 1 3.593 1.327 43.377 43.377 0 0 0-2.011 22.511 1.915 1.915 0 0 1-1.888 2.247z" opacity="1" data-original="#16191e" class=""></path></g></svg>`
-        : `<svg>...</svg>`; // Remplace
+        ? `<svg width="14" height="14" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M7.5 4H16.5C17.12 4 17.67 4.02 18.16 4.09C20.79 4.38 21.5 5.62 21.5 9V15C21.5 18.38 20.79 19.62 18.16 19.91C17.67 19.98 17.12 20 16.5 20H7.5C6.88 20 6.33 19.98 5.84 19.91C3.21 19.62 2.5 18.38 2.5 15V9C2.5 5.62 3.21 4.38 5.84 4.09C6.33 4.02 6.88 4 7.5 4Z" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M13.5 10H17" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M7 15.5H7.02H17" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M10.0941 10H10.1031" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M7.09412 10H7.1031" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+            </svg>`
+        : `<svg width="14" height="14" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M7.5 4H16.5C17.12 4 17.67 4.02 18.16 4.09C20.79 4.38 21.5 5.62 21.5 9V15C21.5 18.38 20.79 19.62 18.16 19.91C17.67 19.98 17.12 20 16.5 20H7.5C6.88 20 6.33 19.98 5.84 19.91C3.21 19.62 2.5 18.38 2.5 15V9C2.5 5.62 3.21 4.38 5.84 4.09C6.33 4.02 6.88 4 7.5 4Z" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M13.5 10H17" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M7 15.5H7.02H17" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M10.0941 10H10.1031" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M7.09412 10H7.1031" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+            </svg>`;
     $: icon2 = $isQuizMode
         ? `<svg width="14" height="14" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M22 4.66994V16.74C22 17.7 21.22 18.5999 20.26 18.7199L19.93 18.76C17.75 19.05 14.39 20.1599 12.47 21.2199C12.21 21.3699 11.78 21.3699 11.51 21.2199L11.47 21.2C9.54997 20.15 6.20003 19.05 4.03003 18.76L3.73999 18.7199C2.77999 18.5999 2 17.7 2 16.74V4.65993C2 3.46993 2.96997 2.56994 4.15997 2.66994C6.25997 2.83994 9.43997 3.89997 11.22 5.00997L11.47 5.15993C11.76 5.33994 12.24 5.33994 12.53 5.15993L12.7 5.04995C13.33 4.65995 14.13 4.26994 15 3.91994V7.99996L17 6.66994L19 7.99996V2.77999C19.27 2.72999 19.53 2.69995 19.77 2.67995H19.83C21.02 2.57995 22 3.46994 22 4.66994Z" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
             <path d="M12 5.48999V20.49" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
             <path d="M19 2.78003V8L17 6.66998L15 8V3.91998C16.31 3.39998 17.77 2.98003 19 2.78003Z" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
             </svg>`
-        : `<svg>...</svg>`;
+        : `<svg width="14" height="14" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M22.0004 17.2C22.0004 18.1 21.7504 18.95 21.3004 19.67C20.4704 21.06 18.9504 22 17.2004 22C15.4504 22 13.9204 21.06 13.1004 19.67C12.6604 18.95 12.4004 18.1 12.4004 17.2C12.4004 14.55 14.5504 12.4 17.2004 12.4C19.8504 12.4 22.0004 14.55 22.0004 17.2Z" stroke="white" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M15.3301 17.2L16.5101 18.38L19.0701 16.02" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M22 8.69C22 10.66 21.49 12.4 20.69 13.91C19.81 12.98 18.57 12.4 17.2 12.4C14.55 12.4 12.4 14.55 12.4 17.2C12.4 18.43 12.87 19.55 13.63 20.4C13.26 20.57 12.92 20.71 12.62 20.81C12.28 20.93 11.72 20.93 11.38 20.81C8.48 19.82 2 15.69 2 8.69C2 5.6 4.49 3.09998 7.56 3.09998C9.37 3.09998 10.99 3.98002 12 5.33002C13.01 3.98002 14.63 3.09998 16.44 3.09998C19.51 3.09998 22 5.6 22 8.69Z" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+            </svg>`;
     $: icon3 = $isQuizMode
         ? `<svg width="14" height="14" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M2.03009 12.42C2.39009 17.57 6.76009 21.76 11.9901 21.99C15.6801 22.15 18.9801 20.43 20.9601 17.72C21.7801 16.61 21.3401 15.87 19.9701 16.12C19.3001 16.24 18.6101 16.29 17.8901 16.26C13.0001 16.06 9.00009 11.97 8.98009 7.13996C8.97009 5.83996 9.24009 4.60996 9.73009 3.48996C10.2701 2.24996 9.62009 1.65996 8.37009 2.18996C4.41009 3.85996 1.70009 7.84996 2.03009 12.42Z" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
             </svg>`
-        : `<svg>...</svg>`;
+        : `<svg width="14" height="14" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M2.03009 12.42C2.39009 17.57 6.76009 21.76 11.9901 21.99C15.6801 22.15 18.9801 20.43 20.9601 17.72C21.7801 16.61 21.3401 15.87 19.9701 16.12C19.3001 16.24 18.6101 16.29 17.8901 16.26C13.0001 16.06 9.00009 11.97 8.98009 7.13996C8.97009 5.83996 9.24009 4.60996 9.73009 3.48996C10.2701 2.24996 9.62009 1.65996 8.37009 2.18996C4.41009 3.85996 1.70009 7.84996 2.03009 12.42Z" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+            </svg>`;
     $: icon4 = $isQuizMode
         ? `<svg width="14" height="14" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M22 12C22 17.52 17.52 22 12 22C6.48 22 3.11 16.44 3.11 16.44M3.11 16.44H7.63M3.11 16.44V21.44M2 12C2 6.48 6.44 2 12 2C18.67 2 22 7.56 22 7.56M22 7.56V2.56M22 7.56H17.56" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
@@ -68,7 +86,10 @@
             <path d="M16 3.5C19.33 3.68 21 4.95 21 9.65V15.83C21 19.95 20 22.01 15 22.01H9C4 22.01 3 19.95 3 15.83V9.65C3 4.95 4.67 3.69 8 3.5H16Z" stroke="white" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
             </svg>
             `
-        : `<svg>...</svg>`;
+        : `<svg width="14" height="14" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M17.2902 4.13999L17.2202 7.92997C17.2102 8.44997 17.5403 9.13999 17.9603 9.44999L20.4403 11.33C22.0303 12.53 21.7703 14 19.8703 14.6L16.6403 15.61C16.1003 15.78 15.5303 16.37 15.3903 16.92L14.6203 19.86C14.0103 22.18 12.4902 22.41 11.2302 20.37L9.47024 17.52C9.15024 17 8.39024 16.61 7.79024 16.64L4.45028 16.81C2.06028 16.93 1.38027 15.55 2.94027 13.73L4.92025 11.43C5.29025 11 5.46024 10.2 5.29024 9.65998L4.28029 6.42997C3.69029 4.52997 4.75028 3.47999 6.64028 4.09999L9.59029 5.06999C10.0903 5.22999 10.8403 5.11998 11.2603 4.80998L14.3403 2.58998C16.0003 1.38998 17.3302 2.08999 17.2902 4.13999Z" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M21.9099 22L18.8799 18.97" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+            </svg>`;
     $: icon6 = $isQuizMode
         ? `<svg width="14" height="14" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M8 2V5" stroke="white" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
@@ -79,10 +100,14 @@
             <path d="M23 17.75C23 18.73 22.72 19.67 22.24 20.45C21.96 20.93 21.61 21.35 21.2 21.69C20.28 22.51 19.08 23 17.75 23C16.6 23 15.54 22.63 14.68 22C14.11 21.59 13.63 21.06 13.26 20.45C12.78 19.67 12.5 18.73 12.5 17.75C12.5 16.1 13.26 14.61 14.47 13.66C15.37 12.93 16.52 12.5 17.75 12.5C18.98 12.5 20.11 12.92 21 13.63C22.22 14.59 23 16.08 23 17.75Z" stroke="white" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
             <path d="M17.75 20.25C17.75 18.87 18.87 17.75 20.25 17.75C18.87 17.75 17.75 16.63 17.75 15.25C17.75 16.63 16.63 17.75 15.25 17.75C16.63 17.75 17.75 18.87 17.75 20.25Z" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
             </svg>`
-        : `<svg>...</svg>`;
+        : `<svg width="14" height="14" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M15.5799 12C15.5799 13.98 13.9799 15.58 11.9999 15.58C10.0199 15.58 8.41992 13.98 8.41992 12C8.41992 10.02 10.0199 8.42004 11.9999 8.42004C13.9799 8.42004 15.5799 10.02 15.5799 12Z" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M11.9998 20.27C15.5298 20.27 18.8198 18.19 21.1098 14.59C22.0098 13.18 22.0098 10.81 21.1098 9.39997C18.8198 5.79997 15.5298 3.71997 11.9998 3.71997C8.46984 3.71997 5.17984 5.79997 2.88984 9.39997C1.98984 10.81 1.98984 13.18 2.88984 14.59C5.17984 18.19 8.46984 20.27 11.9998 20.27Z" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+            </svg>
+            `;
 
     // Title
-    $: title1 = $isQuizMode ? "Clavier LSF" : "Clavier LSF (enfant)";
+    $: title1 = $isQuizMode ? "Clavier LSF" : "Clavier LSF";
     $: title2 = $isQuizMode ? "Dictionnaire" : "Dictionnaire personnalisé";
     $: title3 = $isQuizMode
         ? "Thème clair et foncé"
